752686341c3c22cd92ae222c67131a008875d581
Truptimayee Barik <truptimayee231@gmail.com>
2021-08-31 10:27:44 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log("Scriipt is working in a diffrent file")
+// input
+var username=prompt("Give your username")
+// processing
+var welcomeMessage="This script works "+username;
+// output
+alert(welcomeMessage);
